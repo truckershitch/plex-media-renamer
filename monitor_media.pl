0bd1bed30c5d090bd79ab87524879a54eaae9417
truckershitch <truckershitch@users.noreply.github.com>
2015-01-13 16:25:45 -0500

    Update monitor_media.pl - Added GPL License

--- monitor_media.pl
+++ monitor_media.pl
@@ -1,5 +1,4 @@
 #!/usr/bin/perl
-
 #
 # monitor_media.pl
 # 
@@ -9,12 +8,25 @@
 # Usage: monitor_media.pl <movies|tv>
 # 
 # Version 1.0 -- January 1, 2015
-# truckershitch <truckershitch@users.noreply.github.com>
 #
 # Based on a script created by Ryan Babchishin
 # Ryan Babchishin <rbabchishin@win2ix.ca>
 # http://www.win2ix.ca
 #
+# Copyright 2014, 2015 truckershitch
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # Load the inotify perl module
 use Linux::Inotify2;
